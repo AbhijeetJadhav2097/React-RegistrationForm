0f85eaf1a2db1a9b783fccf5025ea7bf72ba2c5c
Abhijeet Jadhav <abhijeetjadhav2097@gmail.com>
2023-07-29 18:10:46 +0530

    Validation Completed

--- src/component/RegistrationForm/index.js
+++ src/component/RegistrationForm/index.js
@@ -1,99 +1,245 @@
- import { Button, Col, Form, InputGroup, Row} from 'react-bootstrap';
+ import {useState} from 'react'
+ import { Alert, Button, Col, Form, InputGroup, Row} from 'react-bootstrap';
 import './index.css'
 const RegistrationForm = () => { 
+ const [userDetails , setUserDetails] = useState({
+  firstName : '',
+  lastName : '',
+  email: '',
+  city: '',
+  state: '',
+  zipCode: '',
+  tNCAgreed: false
+ })
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+ const [error, setError] = useState({
+  firstName : false,
+  lastName : false,
+  email: false,
+  city: false,
+  state: false,
+  zipCode: false,
+  tNCAgreed: false
+ })
 
-  };
+const [success, setSuccess] = useState(false)
+
+  const handleSubmit = (e) => { 
+      e.preventDefault();  
+
+      const {firstName, lastName, email, city, state, zipCode,tNCAgreed} = userDetails
+
+      if(firstName.length >= 2) {
+        setError((previousError) => ({
+          ...previousError,
+          firstName: false
+        }))
+      } else {
+        setError((previousError) => ({
+          ...previousError,
+          firstName: true
+        }))
+      }
+
+      
+      if(lastName.length >= 2) {
+        setError((previousError) => ({
+          ...previousError,
+          lastName: false
+        }))
+      } else {
+        setError((previousError) => ({
+          ...previousError,
+          lastName: true
+        }))
+      }
+    
+       
+      if (
+        email.includes("@") &&
+        email.includes(".") &&
+        email.indexOf("@") !== 0 &&
+        email.length - email.lastIndexOf(".") >= 3
+      ) {
+        setError((previousError)=>({
+          ...previousError,
+          email: false
+        }))  
+      } else {
+        setError((previousError)=>({
+          ...previousError,
+          email: true
+        }))
+      }
+    
+      if(city.length >= 3) {
+        setError((previousError)=> ({
+          ...previousError,
+          city: false
+        }))
+      } else {
+        setError((previousError) => ({
+          ...previousError,
+          city: true
+        }))
+      }
+
+       
+      if(state.length >=3) {
+        setError((previousError)=>({
+          ...previousError,
+          state: false
+        }))
+      } else {
+        setError((previousError)=>({
+          ...previousError,
+          state: true
+        }))
+      }
+
+ 
+      let zipNumber = parseInt(zipCode)
+      if(zipCode.length === 6 && !isNaN(zipNumber)) {
+        setError((previousError)=>({
+          ...previousError,
+          zipCode: false
+        }))
+      } else {
+        setError((previousError)=>({
+          ...previousError,
+          zipCode: true
+        }))
+      }
+     
+      if(tNCAgreed) {
+        setError((previousError)=>({
+          ...previousError,
+          tNCAgreed: false
+        }))
+      } else {
+        setError((previousError)=>({
+          ...previousError,
+          tNCAgreed: true
+        }))
+      }
+
+    };
 
   return (
     <>
-    <h1 className="display-4" >Sport Registration Form</h1>
+    <h1 className="display-4">Sport Registration Form</h1>
+      {success &&
+        (<Alert variant='success'>
+          Your details were saved successfully!
+        </Alert>)}
     <Form onSubmit={handleSubmit} className="registration-form">
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
           <Form.Label>First name</Form.Label>
           <Form.Control
-            required
+            value={userDetails.firstName} 
+            onChange={(e)=>setUserDetails({
+              ...userDetails,
+              firstName: e.target.value
+            })}
             type="text"
-            placeholder="First name"
-            defaultValue="Mark"
+            placeholder="First name" 
           />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type='invalid'>Looks good!</Form.Control.Feedback>
+          {error.firstName ? (<Form.Control.Feedback type='invalid'>Please enter valid first Name</Form.Control.Feedback>) : ( <Form.Control.Feedback>Looks good!</Form.Control.Feedback>)}
+         
+          
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustom02">
           <Form.Label>Last name</Form.Label>
           <Form.Control
-            required
+            value={userDetails.lastName}
+            onChange={(e)=> setUserDetails({
+              ...userDetails,
+              lastName: e.target.value
+            })}
             type="text"
-            placeholder="Last name"
-            defaultValue="Otto"
+            placeholder="Last name" 
           />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type='invalid'>Looks good!</Form.Control.Feedback>
-
+            {error.lastName ? (<Form.Control.Feedback type='invalid'>Please enter valid Last Name</Form.Control.Feedback>) : (<Form.Control.Feedback>Looks good!</Form.Control.Feedback>)}
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
+          <Form.Label>Email</Form.Label>
           <InputGroup hasValidation>
             <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
             <Form.Control
               type="text"
-              placeholder="Username"
+              placeholder="Email"
               aria-describedby="inputGroupPrepend"
-              required
-            />        
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid">
-              Please choose a username.
-            </Form.Control.Feedback>
+             value={userDetails.email}
+             onChange={(e)=>setUserDetails({
+              ...userDetails,
+              email: e.target.value
+             })}
+            />      
+            {error.email ? (<Form.Control.Feedback type="invalid">
+              Please enter a valid email
+            </Form.Control.Feedback>) : (  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>)}  
           </InputGroup>
         </Form.Group>
       </Row>
       <Row className="mb-3">
         <Form.Group as={Col} md="6" controlId="validationCustom03">
           <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
+          <Form.Control type="text" placeholder="City" 
+            value={userDetails.city}
+            onChange={(e)=>setUserDetails({
+              ...userDetails,
+              city: e.target.value
+            })}
+           />
+           {error.city ? ( <Form.Control.Feedback type="invalid">
             Please provide a valid city.
-          </Form.Control.Feedback>
+          </Form.Control.Feedback>) : ( <Form.Control.Feedback>Looks good!</Form.Control.Feedback>)}     
         </Form.Group>
         <Form.Group as={Col} md="3" controlId="validationCustom04">
           <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
+          <Form.Control type="text" placeholder="State" 
+            value={userDetails.state}
+            onChange={(e)=> setUserDetails({
+              ...userDetails,
+              state:e.target.value
+            })}
+          />
+          {error.state ? ( <Form.Control.Feedback type="invalid">
+            Please enter a valid state.
+          </Form.Control.Feedback>) : ( <Form.Control.Feedback>Looks good!</Form.Control.Feedback>)}  
         </Form.Group>
         <Form.Group as={Col} md="3" controlId="validationCustom05">
           <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
+          <Form.Control type="text" placeholder="Zip" 
+            value={userDetails.zipCode}
+            onChange={(e)=>setUserDetails({
+              ...userDetails,
+              zipCode:e.target.value
+            })}
+          />
+          {error.zipCode ? ( <Form.Control.Feedback type="invalid">
+            Please enter a valid zip.
+          </Form.Control.Feedback>) : ( <Form.Control.Feedback>Looks good!</Form.Control.Feedback>)}
         </Form.Group>
       </Row>
-      <Form.Group className="mb-3">
+      <Form.Group className="mb-3" style={{textAlign: 'left'}}>
         <Form.Check
-          required
-          label="Agree to terms and conditions"
-          feedback="You must agree before submitting."
-          feedbackType="invalid"
+          
+          label="Agree to terms and conditions" 
+          checked={userDetails.tNCAgreed}
+          onChange={() => setUserDetails({
+            ...userDetails,
+            tNCAgreed: !userDetails.tNCAgreed,
+          })}
         />
+        {error.tNCAgreed && (<Form.Control.Feedback type="invalid">
+            You need to agree to terms and conditions
+          </Form.Control.Feedback>)}
       </Form.Group>
       <Button type="submit">Submit form</Button>
     </Form>
     </>
   );
-}
-
+ }
 export default RegistrationForm;
